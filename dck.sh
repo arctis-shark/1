41a2d6bc5db3bcf2261794303b0e3e5798902caf
arctis <78988923+arctis-shark@users.noreply.github.com>
2023-06-26 13:46:57 -0400

    Update dck.sh

--- dck.sh
+++ dck.sh
@@ -2,7 +2,7 @@ sudo docker run -d \
   --name homeassistant \
   --privileged \
   --restart=unless-stopped \
-  -e TZ=MY_TIME_ZONE \
-  -v /PATH_TO_YOUR_CONFIG:/config \
+  -e TZ=America/Toronto \
+  -v /home/hass/hass-config:/config \
   --network=host \
   ghcr.io/home-assistant/home-assistant:stable
